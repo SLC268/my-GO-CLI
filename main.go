8f229bfae7230cd840c44228a17ab5f9803bb43b
Sonya <sonya.clausen@hotmail.com>
2024-10-10 09:41:05 +0200

    added cobra

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 func main() {
-	fmt.Println("It Works!")
+	var rootCmd = &cobra.Command{
+		Use:   "myapp",
+		Short: "My GO CLI application",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("It Works!")
+		},
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		return
+	}
 }
